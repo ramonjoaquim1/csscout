cb67ebdc10ee0726948029522ec293e61de87c16
ramoncheroso <ramon_joaquim1@hotmail.com>
2024-01-02 19:02:48 -0300

    CS Scout

---
+++ src/main/java/br/com/csscout/CsscoutApplication.java
@@ -0,0 +1,13 @@
+package br.com.csscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsscoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/csscout/csscout/CsscoutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.csscout.csscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsscoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
