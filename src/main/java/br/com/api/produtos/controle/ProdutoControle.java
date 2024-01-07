19665049593372f344e4d6e09bccbb7bf3d298ed
ramoncheroso <ramon_joaquim1@hotmail.com>
2024-01-06 21:29:58 -0300

    React adicionado e remover produto

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -2,10 +2,14 @@ package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,11 +17,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController //isso cria Rotas
-
+@CrossOrigin(origins = "*")
 public class ProdutoControle{
 
     @Autowired
     private ProdutoServico ps;
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
+
 
     @PutMapping("/alterar")
     public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {   
