263ea50b2a0a04bb7081b2e21f114f6aac66e51f
ramoncheroso <ramon_joaquim1@hotmail.com>
2024-01-27 12:52:07 -0300

    commit ok

--- src/main/java/br/com/api/CsscoutApplication.java
+++ src/main/java/br/com/api/CsscoutApplication.java
@@ -2,7 +2,7 @@ package br.com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+
 @SpringBootApplication
 public class CsscoutApplication {
 
